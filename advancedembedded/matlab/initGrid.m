95dacf8537bc2972c1d501684cdb4ae382f1502d
Ryan Spangler <rspangler@c-98-246-170-166.hsd1.or.comcast.net>
2010-05-11 17:45:03 -0700

    forgot to git mv

--- advancedembedded/matlab/initGrid.m
+++
@@ -1,6 +0,0 @@
-function grid = initGrid(xMax, yMax)
-% initGrid - Create an empty grid with dimensions xMax and yMax
-
-grid.dimension = [xMax, yMax];
-grid.slots = xMax * yMax;
-grid.particleMatrix = zeros(yMax, xMax);
--- advancedembedded/matlab/runGrid.m
+++
@@ -1,5 +0,0 @@
-function grid = runGrid(grid)
-% runGrid - Run the given particle grid for one time step.
-
-chosen = randi(length(grid.groups));
-group = grid.groups(chosen);
