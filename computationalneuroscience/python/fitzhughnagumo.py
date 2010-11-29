dcddbe8d150ebd8fd43f8d4224e5ecd054a8167d
Ryan Spangler <rspangler@ryan-spangler.lan>
2010-11-29 02:07:20 -0800

    phase plane diagrams

--- computationalneuroscience/python/fitzhughnagumo.py
+++ computationalneuroscience/python/fitzhughnagumo.py
@@ -23,3 +23,37 @@ class FitzhughNagumo(Integration):
         """the eigenvalues for the system given a particular V"""
         return roots([1, (10 * pow(V, 2) - 8), 20 * pow(V, 2)])
 
+    def plotVR(self, begin, end, step):
+        self.run(begin, end, step)
+        plt.plot(self.traces['V'], self.traces['R'])
+
+def plot_behavior(begin=0, end=10, dt=0.01):
+    # equilibrium = FitzhughNagumo(I=lambda t: 0.6)
+    # spiking = FitzhughNagumo(I=lambda t: 0.7)
+    equilibrium = FitzhughNagumo(I=lambda t: 1.0)
+    spiking = FitzhughNagumo(I=lambda t: 1.1)
+
+    for model in [equilibrium, spiking]:
+        model.run(begin, end, dt)
+
+    def rnullcline(V):
+        return 1.25 * V + 1.5
+
+    def vnullcline(V, I):
+        return V - pow(V, 3) * 0.33333333 + I
+
+    base = arange(-2.2, 2.2, dt)
+
+    plt.plot(spiking.traces['V'], spiking.traces['R'])
+    plt.plot(equilibrium.traces['V'], equilibrium.traces['R'])
+    plt.plot(base, map(rnullcline, base))
+    # plt.plot(base, map(lambda V: vnullcline(V, 0.7), base))
+    # plt.plot(base, map(lambda V: vnullcline(V, 0.6), base))
+    plt.plot(base, map(lambda V: vnullcline(V, 1.1), base))
+    plt.plot(base, map(lambda V: vnullcline(V, 1.0), base))
+
+    plt.legend(['I=1.1', 'I=1.0'], loc='lower right')
+    # plt.legend(['I=0.7', 'I=0.6'], loc='lower right')
+    plt.axis([-2.2, 2.2, -0.75, 2.5])
+    plt.xlabel('V')
+    plt.ylabel('R')
