9cf89ce5868b560146efc9cb29622815604e1bb9
Ryan Spangler <rspangler@ryan-spangler.lan>
2010-12-10 05:25:27 -0800

    figures and simulation

--- computationalneuroscience/python/sompolinsky.py
+++ computationalneuroscience/python/sompolinsky.py
@@ -2,7 +2,7 @@ from pylab import *
 from IPython.Debugger import Tracer; debug = Tracer()
 
 class Sompolinsky:
-    def __init__(self, fields=3, dimensions=3, granularity=4, topology=[[1, 1, 0], [1, 1, 1], [0, 1, 1]], tau=1.0, sigma=1.0, strong=1.0, weak=1.0, order=0.1):
+    def __init__(self, fields=3, dimensions=3, granularity=4, topology=[[1, 1, 0], [1, 1, 1], [0, 1, 1]], tau=0.1, sigma=1.0, strong=1.0, weak=1.0, base=0.04, order=0.01):
         """ Sompolinsky: a general framework for mutually synchronizing interconnected receptive fields.
               fields -- the number of general receptive fields (composed of groups of neurons).
               dimensions -- the number of distinct types of stimuli the system responds to.
@@ -11,8 +11,11 @@ class Sompolinsky:
                 matrix showing the connectivity between each field.  The diagonal (self-connection) is always 
                 assumed to be 1.
               tau -- the time constant of change
+              sigma -- the rate of intolerance to other stimulus orientations
               strong -- the strength of connections within a receptive field
-              weak -- the strength of a connection between receptive fields """
+              weak -- the strength of a connection between receptive fields 
+              base -- the minimum frequency 
+              order -- the span of frequency values """
         self.N = int(fields * dimensions * granularity)
         self.radius = int(dimensions * granularity)
         self.fields = fields
@@ -23,7 +26,7 @@ class Sompolinsky:
         self.sigma = sigma
         self.strong = strong / self.N
         self.weak = weak / pow(self.N, 2)
-        self.inverseN = 1.0 / self.N
+        self.base = base
         self.order = order
 
         self.orientations = self.generate_orientations()
@@ -33,18 +36,19 @@ class Sompolinsky:
         self.reset()
 
     def reset(self):
+        """ restore the model to its initial conditions. """
         self.stimuli = zeros(self.dimensions)
         self.present = zeros(self.dimensions)
         self.activity = zeros(self.N)
 
         self.phase = random(self.N) * 2*pi
-        self.intrinsic = random(self.N) * self.order
+        self.intrinsic = random(self.N) * self.order + self.base
         self.spikes = zeros(self.N)
         self.phases = self.phase
 
     def insert_granularity(self, dimension):
         """ give a group of neurons corresponding to a particular dimension of stimulus their respective 
-            sensitivities"""
+            sensitivities. """
         basis = tile(-1.0, [self.dimensions, self.granularity])
         basis[dimension] = self.orientations
         return basis.T
@@ -83,35 +87,42 @@ class Sompolinsky:
     def tuning(self, field, stimulus):
         """ given a field of selectivities and a stimulus, return a vector composed of that field's 
             responses to the stimulus. """
-        return self.V(where(field > 0, field - stimulus, 10000)).sum(axis=1)
+        return self.V(mod(where(field >= 0, field - stimulus, 10000), 2*pi)).sum(axis=1)
 
     def trigger_activity(self, stimuli):
         """ iterate over the distinct receptive fields with their corresponding stimulus to determine
             the activities of these neurons this time step. """
         self.present = stimuli
-        self.responses = []
+        responses = []
         for receptive, stimulus in zip(range(self.fields), stimuli):
             field = self.selectivities[(receptive * self.radius):((receptive + 1) * self.radius)]
-            self.responses.append(self.tuning(field, stimulus))
-        self.activity = hstack(self.responses)
+            responses.append(self.tuning(field, stimulus))
+        return hstack(responses)
             
     def V(self, thetas):
+        """ the tuning curve for the selectivity of a neuron """
         return exp(-abs(thetas)) * self.sigma
 
     def J(self, r):
+        """ the connectivity function """
         return self.activity[r] * self.connectivities[r] * self.activity
 
+    def noise(self, size, width=0.1):
+        return random(size) * width - (width * 0.5)
+
     def deltar(self, r):
-        return self.intrinsic[r] + sum(self.J(r) * sin(self.phase - self.phase[r]))
+        """ the change in phase for a single neuron. """
+        return self.intrinsic[r] + sum(self.J(r) * sin(self.phase - self.phase[r])) * self.tau
 
     def delta(self):
+        """ the phase function for all neurons in the system. """
         return array(map(self.deltar, range(self.N)))
 
     def step(self, now):
         """ go through a single time step of the system.  now is a vector of size self.dimensions 
             representing the stimulus for this time step. """
         self.stimuli = vstack([self.stimuli, now])
-        self.trigger_activity(now)
+        self.activity = self.trigger_activity(now)
         self.phase += self.delta()
 
         self.spikes = vstack([self.spikes, where(self.phase >= 2*pi, 1, 0)])
@@ -119,6 +130,8 @@ class Sompolinsky:
         self.phases = vstack([self.phases, self.phase])
 
     def run(self, steps):
+        """ run the simulation for the given stream of stimuli.  
+            steps must be a list of vectors for each field each of which has the same size as the model's dimension. """
         self.reset()
         for step in steps:
             self.step(step)
