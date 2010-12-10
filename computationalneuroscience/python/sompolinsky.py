f6a0a080c6f0e33c0b19155372e290571d269430
Ryan Spangler <rspangler@Ryan-Spangler.local>
2010-12-09 17:40:58 -0800

    sompolinsky's model of the primary visual cortex

--- computationalneuroscience/python/sompolinsky.py
+++ computationalneuroscience/python/sompolinsky.py
@@ -1,27 +1,67 @@
 from pylab import *
+from IPython.Debugger import Tracer; debug = Tracer()
 
 class Sompolinsky:
-    def __init__(self, fields=2, dimensions=3, granularity=4, tau=0.1):
+    def __init__(self, fields=3, dimensions=3, granularity=4, topology=[[1, 1, 0], [1, 1, 1], [0, 1, 1]], tau=0.1, strong=0.5, weak=0.1):
+        """ Sompolinsky: a general framework for mutually synchronizing interconnected receptive fields.
+              fields -- the number of general receptive fields (composed of groups of neurons).
+              dimensions -- the number of distinct types of stimuli the system responds to.
+              granularity -- how many bins one type of stimuli is divided into.
+              topology -- how the fields are connected to one another.  This takes the form of a FIELDSxFIELDS
+                matrix showing the connectivity between each field.  The diagonal (self-connection) is always 
+                assumed to be 1.
+              tau -- the time constant of change
+              strong -- the strength of connections within a receptive field
+              weak -- the strength of a connection between receptive fields """
+
         self.N = fields * dimensions * granularity
         self.fields = fields
         self.dimensions = dimensions
         self.granularity = granularity
+        self.topology = topology
         self.tau = tau
+        self.strong = strong
+        self.weak = weak
         self.inverseN = 1.0 / self.N
         self.orientations = linspace(0, (granularity-1) * 2 * pi / granularity, granularity)
 
+        self.generate_selectivities()
+        self.generate_connectivities()
+
+        self.reset()
+
+    def insert_granularity(self, dimension):
+        basis = tile(-1, [self.dimensions, self.granularity])
+        basis[dimension] = self.orientations
+        return basis.T
+
+    def generate_selectivities(self):
+        """ stimuli come in as a value between 0 and 2pi.
+            -1 represents no selectivity. """
         selectivities = []
         for field in range(self.fields):
             dimensions = map(lambda dimension: self.insert_granularity(dimension), range(self.dimensions))
             selectivities.append(vstack(dimensions))
         self.selectivities = vstack(selectivities)
 
-        self.reset()
+    def generate_connectivities(self):
+        connectivities = zeros([self.N, self.N])
+        receptive = self.dimensions * self.granularity
+        for field, connections in zip(range(self.fields), self.topology):
+            for connection, connectivity in zip(range(self.fields), connections):
+                begin = [receptive * field, receptive * connection]
+                end = [receptive * (field + 1), receptive * (connection + 1)]
 
-    def insert_granularity(self, dimension):
-        basis = zeros([self.dimensions, self.granularity])
-        basis[dimension] = self.orientations
-        return basis.T
+                if field == connection:
+                    grid = tile(self.strong, (receptive, receptive))
+                elif connectivity == 1:
+                    grid = eye(receptive) * self.weak
+                else:
+                    grid = zeros([receptive, receptive])
+                
+                connectivities[begin[0]:end[0], begin[1]:end[1]] = grid
+
+        self.connectivities = connectivities
 
     def reset(self):
         self.phase = random(self.N) * 2 * pi
